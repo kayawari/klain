def4eaa6d84477aebf75e93b69e2de4f56f8701a
takayuki.kayawari <kayamelo@gmail.com>
2018-09-15 23:26:33 +0900

    add Kpt edit action

--- app/controllers/kpts_controller.rb
+++ app/controllers/kpts_controller.rb
@@ -8,11 +8,25 @@ class KptsController < ApplicationController
   end
 
   def create
-    @kpt = Kpt.new(kpt_params)
-    if @kpt.save
+    kpt = Kpt.new(kpt_params)
+    if kpt.save
       redirect_to kpts_path, notice: "Success to create new KPT project"
     else
       flash[:alert] = "Failed to create new KPT project"
+      render :new
+    end
+  end
+
+  def edit
+    @kpt = Kpt.find(params[:id])
+  end
+
+  def update
+    kpt = Kpt.find(params[:id])
+    if kpt.update(kpt_params)
+      redirect_to kpts_path, notice: "Success to update KPT project"
+    else
+      flash[:alert] = "Failed to update KPT project"
       render :new
     end
   end
